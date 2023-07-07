cd984d1403a53c0557bc60fe207f89502b211f3d
theProficient <theproficienttech333@gmail.com>
2023-07-07 18:37:15 +0530

    changes in css file

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -21,7 +21,7 @@ const gamesCardData = [
     {
         imgsrc: game3,
         title:"TicTacToe",
-        text:"Tic-Tac-Toe is traditionally played on a 3 × 3 grid. Players take turns placing a mark in one of the cells of the grid. The goal of the game is for players to position their marks so that they make a continuous line of three cells vertically, horizontally, or diagonally.",
+        text:"Tic-Tac-Toe is traditionally played on a 3 × 3 grid. Players take turns placing a mark in one of the cells of the grid. The goal of the game is for players to position their marks so that they make a continuous line of three cells.",
         view:"/TicTacToe"
     },
 
