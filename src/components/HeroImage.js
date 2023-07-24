01482ef44ceb538478e6dda24baf67effe200f6a
theProfficient <theproficienttech333@gmail.com>
2023-07-24 15:13:46 +0530

    change bg img

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,7 +2,7 @@ import "./HeroImageStyle.css";
 
 import React from 'react'
 
-import IntroImage from "../assets/blue1.jpg"
+import IntroImage from "../assets/blue2.jpg"
 import { Link } from "react-router-dom";
 
 const HeroImage = () => {
